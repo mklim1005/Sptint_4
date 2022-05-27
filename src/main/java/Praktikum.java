d08988142fd1a47e7856aa28382327563c1f6a7c
Marina Klimova <m.klim1005@gmail.com>
2022-05-27 23:38:11 +0300

    Implemented main method

--- src/main/java/Praktikum.java
+++ src/main/java/Praktikum.java
@@ -1,11 +1,14 @@
 public class Praktikum {
 
     public static void main(String[] args) {
-        /*
-             В этом методе заложи логику работы с классом Account.
-             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
-             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
-         */
+
+        String name = "Vinni Puh";
+        Account account = new Account(name);
+        if (account.checkNameToEmboss()) {
+            System.out.println("Your name will be printed");
+        } else {
+            System.out.println("Incorrect name");
+        }
     }
 
 }
